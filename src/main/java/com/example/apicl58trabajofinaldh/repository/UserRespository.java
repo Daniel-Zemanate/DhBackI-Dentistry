e66fdff7282bcad8cb2ce321231579aba78677d5
Daniel-Zemanate <zemanateaguirre@gmail.com>
2022-07-07 11:27:41 -0500

    added some anotations

--- src/main/java/com/example/apicl58trabajofinaldh/repository/UserRespository.java
+++ src/main/java/com/example/apicl58trabajofinaldh/repository/UserRespository.java
@@ -2,9 +2,13 @@ package com.example.apicl58trabajofinaldh.repository;
 
 import com.example.apicl58trabajofinaldh.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface UserRespository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername (String username);
